78c44a19925a6ffc193424891d8a34f59a961847
nuthansamarth <2024cs_nuthansamarthah_c@nie.ac.in>
2025-08-16 22:44:50 +0530

    hcf and lcm

---
+++ hcf.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class hcf {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        System.out.println("enter a: ");
+        int a=scn.nextInt();
+        int a1=a;
+        System.out.println("enter b: ");
+        int b=scn.nextInt();
+        int b1=b;
+        scn.close();
+        while (b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        System.out.println("hcf is: "+a);
+        int lcm =(a1*b1)/a;             //lcm(a,b)*hcf(a,b)=a*b      //hcf(a,b)=hcf(a,r) r=a%b
+        System.out.println("the lcm is: "+ lcm);
+        
+    }
+    
+}
