39c18df1407137504d38920242f96d2d727ba0d2
nuthansamarth <2024cs_nuthansamarthah_c@nie.ac.in>
2025-08-17 12:26:30 +0530

    inverted star

--- inverted_star.java
+++ inverted_star.java
@@ -1,18 +1,17 @@
-import java.util.*;;
-public class star {
+import java.util.*;
+public class inverted_star {
     public static void main(String[] args) {
-        System.out.println("enter number of rows: ");
+        System.out.println("enter the number of rows: ");
         Scanner scn=new Scanner(System.in);
         int rows=scn.nextInt();
         scn.close();
         for(int i=1;i<=rows;i++){
             System.out.print("\n");
-            for (int j=1;j<=i;j++){
+            for(int j=1;j<=rows-i+1;j++){
                 System.out.print("*");
-                
-
             }
-            
+       
         }
+
     }
 }
