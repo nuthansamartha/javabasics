51f2856b58aa299edd6d9ca05080ccc6f60c742c
nuthansamarth <2024cs_nuthansamarthah_c@nie.ac.in>
2025-08-17 11:37:20 +0530

    star pattern

---
+++ star.java
@@ -0,0 +1,17 @@
+import java.util.*;;
+public class star {
+    public static void main(String[] args) {
+        System.out.println("enter number of rows: ");
+        Scanner scn=new Scanner(System.in);
+        int rows=scn.nextInt();
+        for(int i=1;i<=rows;i++){
+            System.out.print("\n");
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+                
+
+            }
+            
+        }
+    }
+}
